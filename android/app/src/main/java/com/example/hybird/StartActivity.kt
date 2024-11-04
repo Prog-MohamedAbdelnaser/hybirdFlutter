cc96544e184b16487bdd4fdab8db9ade47735846
mohamedabdelnaser <mohamedabdelnaser782@gmail.com>
2024-11-04 21:11:04 +0200

    init

---
+++ android/app/src/main/java/com/example/hybird/StartActivity.kt
@@ -0,0 +1,32 @@
+package com.example.hybird
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_start)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<MaterialButton>(R.id.btnRepo).setOnClickListener {
+            navigateToFlutter()
+        }
+    }
+
+    private fun navigateToFlutter() {
+        val intent = Intent(this, MainActivity::class.java)
+
+        startActivity(intent)
+    }
+}
---
+++ android/app/src/main/kotlin/com/example/hybird/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hybird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
